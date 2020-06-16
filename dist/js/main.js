907702c6d44ba0ea8fdcc6f999abc175932894d5
moulichan99 <moulichan99@gmail.com>
2020-06-17 02:33:18 +0530

    second

---
+++ dist/js/main.js
@@ -0,0 +1,30 @@
+const menubtn = document.querySelector('.menu-btn');
+const menu = document.querySelector('.menu');
+const menuBranding = document.querySelector('.menu-branding');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.nav-items');
+
+
+let showMenu = false;
+
+menubtn.addEventListener('click',toggleMenu);
+
+function toggleMenu(){
+	if(!showMenu){
+		menubtn.classList.add('close');
+		menu.classList.add('show');
+		menuBranding.classList.add('show');
+		menuNav.classList.add('show');
+		navItems.forEach(item => item.classList.add('show'));
+		showMenu = true;
+	}
+	else{
+		menubtn.classList.remove('close');
+		menu.classList.remove('show');
+		menuBranding.classList.remove('show');
+		menuNav.classList.remove('show');
+		navItems.forEach(item => item.classList.remove('show'));
+		showMenu = false;
+	}
+
+}
